9972172d847e31ee783e91ca97dd305c494495d3
Mark Constable <markc@renta.net>
2025-01-28 20:19:46 +1000

    Changes for typesafe and PHP 8.4

--- php/plugins/processes.php
+++ php/plugins/processes.php
@@ -4,13 +4,19 @@ declare(strict_types=1);
 // plugins/processes.php 20170225 - 20260128
 // Copyright (C) 2015-2025 Mark Constable <markc@renta.net> (AGPL-3.0)
 
-class Plugins_Processes extends Plugin
+final class Plugins_Processes extends Plugin
 {
     public function list(): string
     {
         elog(__METHOD__);
 
         $cmd = "ps -eo rss:10,vsz:10,%cpu:5,cmd --sort=rss | grep -v \"^\s\+0\" | cut -c -79";
-        return $this->t->list(['procs' => shell_exec($cmd)]);
+        $output = shell_exec($cmd);
+
+        if ($output === null || $output === false) {
+            return $this->t->list(['procs' => 'Error: Unable to fetch process information']);
+        }
+
+        return $this->t->list(['procs' => $output]);
     }
 }
